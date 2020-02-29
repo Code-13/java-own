c7eb9a3ca9b1177f1b89e9e856e603a542339619
code13 <kangqian.lv@outlook.com>
2020-02-29 13:40:00 +0800

    feature(common): 添加通用工具类

---
+++ java-common/common-util/src/main/java/com/code13/util/number/NumberConverter.java
@@ -0,0 +1,98 @@
+package com.code13.util.number;
+
+/**
+ * @author code13
+ * @date 2020-02-23 17:36
+ */
+public class NumberConverter {
+
+  /// <summary>
+  /// 阿拉伯數字轉中文
+  /// </summary>
+  /// <param name="intInput"></param>
+  /// <returns></returns>
+  public static String ToConvtZH(Integer intInput) {
+    String sd = toCn(intInput);
+    String sd2 = sd;
+    if (intInput == 10) {
+      //sd2 = sd.insert(sd.lastIndexOf('十'), "一");
+      sd2 = "十";
+    }
+    return sd2;
+  }
+
+  public static String toCn(Integer intInput) {
+    String si = intInput.toString();
+    String sd = "";
+    if (si.length() == 1) {
+      sd += getCn(intInput);
+      return sd;
+    } else if (si.length() == 2) {
+      if (si.substring(0, 1) == "1") {
+        sd += "十";
+      } else {
+        sd += (getCn(intInput / 10) + "十");
+      }
+      sd += toCn(intInput % 10);
+    } else if (si.length() == 3) {
+      sd += (getCn(intInput / 100) + "百");
+      if (String.valueOf(intInput % 100).length() < 2 && (intInput % 100) != 0) {
+        sd += "零";
+      }
+      sd += toCn(intInput % 100);
+    } else if (si.length() == 4)//千
+    {
+      sd += (getCn(intInput / 1000) + "千");
+      if (String.valueOf(intInput % 1000).length() < 3 && (intInput % 1000) != 0) {
+        sd += "零";
+      }
+      sd += toCn(intInput % 1000);
+    } else if (si.length() == 5)//萬
+    {
+      sd += (getCn(intInput / 10000) + "万");
+      if (String.valueOf(intInput % 10000).length() < 4 && (intInput % 10000) != 0) {
+        sd += "零";
+      }
+      sd += toCn(intInput % 10000);
+    }
+
+    return sd;
+  }
+
+  private static String getCn(int input) {
+    String sd = "";
+    switch (input) {
+      case 1:
+        sd = "一";
+        break;
+      case 2:
+        sd = "二";
+        break;
+      case 3:
+        sd = "三";
+        break;
+      case 4:
+        sd = "四";
+        break;
+      case 5:
+        sd = "五";
+        break;
+      case 6:
+        sd = "六";
+        break;
+      case 7:
+        sd = "七";
+        break;
+      case 8:
+        sd = "八";
+        break;
+      case 9:
+        sd = "九";
+        break;
+      default:
+        break;
+    }
+    return sd;
+  }
+
+}
--- java-core/java-enum/src/test/java/com/code13/javaenum/SpringBootJavaEnumApplicationTests.java
+++ java-core/java-enum/src/test/java/com/code13/javaenum/SpringBootJavaEnumApplicationTests.java
@@ -1,6 +1,12 @@
 package com.code13.javaenum;
 
+import com.google.common.collect.Lists;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 枚举测试
@@ -11,5 +17,55 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class SpringBootJavaEnumApplicationTests {
 
+  @Test
+  public void test1() {
+    String str = "21-1-1-2-201";
+    String[] split = str.split("-");
+
+    List<String> list = Lists.newArrayList(split);
+    Collections.reverse(list);
+    List<String> subList = list.subList(0, 4);
+
+    System.out.println(list);
+    System.out.println(subList);
+  }
+
+  @Test
+  public void test2() {
+    String str = "21-1-1-2-201";
+    String[] split = str.split("-");
+
+    String[] strings = Arrays.copyOfRange(split, split.length - 4, split.length);
+
+    System.out.println(Arrays.toString(strings));
+
+  }
+
+  @Test
+  public void test3() {
+    String uniqueId = "21-1-1-2-201";
+    String[] split = uniqueId.split("-");
+    String[] strings = Arrays.copyOfRange(split, split.length - 4, split.length);
+
+    List<String> list = Lists.newArrayList(strings);
+
+    for (int i = 0; i < list.size(); i++) {
+      String s = list.get(i);
+      if (i == 0) {
+        s = s + "号楼";
+      } else if (i == 1) {
+        s = s + "单元";
+      } else if (i == 2) {
+        s = s + "F";
+      }
+      list.set(i, s);
+    }
+
+    System.out.println(list);
+
+    String collect = String.join("-", list);
+
+    System.out.println(collect);
+  }
 
 }
