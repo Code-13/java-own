94f297eac1985a2913cd27cb4f0eeadeafef562a
code13 <code__13@outlook.com>
2020-09-04 17:54:55 +0800

    :bug: Oauth2
    修复不能检查的问题

--- design-patterns/strategy/src/main/java/com/github/code13/designpatterns/strategy/demo02/annotation/OrderHandlerTypeImpl.java
+++ design-patterns/strategy/src/main/java/com/github/code13/designpatterns/strategy/demo02/annotation/OrderHandlerTypeImpl.java
@@ -1,6 +1,15 @@
 package com.github.code13.designpatterns.strategy.demo02.annotation;
 
+import com.github.code13.designpatterns.strategy.demo02.handler.MobileOrderHandler;
+
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 订单处理器类型实现
@@ -53,4 +62,32 @@ public class OrderHandlerTypeImpl implements OrderHandlerType {
     OrderHandlerType other = (OrderHandlerType) obj;
     return this.source.equals(other.source()) && this.payMethod.equals(other.payMethod());
   }
+
+  public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+
+    List<String> strings = Arrays.asList("equals", "toString", "hashCode", "annotationType");
+
+    final OrderHandlerType annotation = MobileOrderHandler.class.getAnnotation(OrderHandlerType.class);
+
+    final Method[] methods = OrderHandlerType.class.getMethods();
+
+    for (Method method : methods) {
+      System.out.println(method.getName());
+    }
+
+    final Set<Method> methodSet = Arrays.stream(methods).filter(method -> !strings.contains(method.getName())).collect(Collectors.toSet());
+
+    final Map<String, Object> collect1 = methodSet.stream().collect(Collectors.toMap(Method::getName, method -> {
+      try {
+        return method.invoke(annotation);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      return null;
+    }, (o, o2) -> o));
+
+    System.out.println(collect1);
+
+  }
+
 }
