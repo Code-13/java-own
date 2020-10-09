0f5e08cc116d6a12eaba5288d068844d3615caeb
code13 <code__13@outlook.com>
2020-10-09 10:37:28 +0800

    :sparkles: ap
    ap

---
+++ java-core/java-annotation-processor/src/main/java/com/github/code13/ap/AnnotationProcessorApplication.java
@@ -0,0 +1,19 @@
+package com.github.code13.ap;
+
+/**
+ * @author Code13
+ * @date 2020-10-09 09:11
+ */
+//@SpringBootApplication
+public class AnnotationProcessorApplication {
+
+  //public static void main(String[] args) {
+  //  //SpringApplication.run(AnnotationProcessorApplication.class, args);
+  //  final ServiceLoader<Processor> processors = ServiceLoader.load(Processor.class);
+  //  for (Processor processor : processors) {
+  //    System.out.println(processor.getClass().getName());
+  //  }
+  //
+  //}
+
+}
--- java-core/java-annotation-processor/src/test/java/com/github/code13/ap/AnnotationProcessorTest.java
+++ java-core/java-annotation-processor/src/test/java/com/github/code13/ap/AnnotationProcessorTest.java
@@ -1,9 +1,14 @@
 package com.github.code13.ap;
 
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 import org.junit.Test;
 
 import javax.annotation.processing.Processor;
-import java.util.Iterator;
+import javax.lang.model.element.Modifier;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ServiceLoader;
 
 /**
@@ -15,9 +20,33 @@ public class AnnotationProcessorTest {
   @Test
   public void test() {
     final ServiceLoader<Processor> processors = ServiceLoader.load(Processor.class);
-    final Iterator<Processor> iterator = processors.iterator();
-    while (iterator.hasNext()) {
-      iterator.next();
+    for (Processor processor : processors) {
+      System.out.println(processor.getClass().getName());
+    }
+  }
+
+  @Test
+  public void test2() {
+    MethodSpec main = MethodSpec.methodBuilder("main")
+      .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+      .returns(void.class)
+      .addParameter(String[].class, "args")
+      .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+      .build();
+    // HelloWorld class
+    TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+      .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+      .addMethod(main)
+      .build();
+    try {
+      // build com.example.HelloWorld.java
+      JavaFile javaFile = JavaFile.builder("com.github.code13.ap", helloWorld)
+        .addFileComment(" This codes are generated automatically. Do not modify!")
+        .build();
+      // write to file
+      javaFile.writeTo(Path.of("D:\\code13\\own\\java-own\\java-core\\java-annotation-processor\\src\\main\\java"));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 
